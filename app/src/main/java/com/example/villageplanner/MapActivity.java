28fdb34aaf45650212a95458bbee3dfdcefcbe25
Isabella <irocha@usc.edu>
2022-10-23 19:58:11 -0700

    Initial functions

--- app/src/main/java/com/example/villageplanner/MapActivity.java
+++ app/src/main/java/com/example/villageplanner/MapActivity.java
@@ -1,4 +1,14 @@
 package com.example.villageplanner;
 
+import java.util.List;
+
 public class MapActivity {
+    private List<DestinationActivity> destinations;
+
+    public void displayRoute() {}
+    public void calculateRoute() {}
+    public void displayDestinationInfo() {}
+    public List<DestinationActivity> getDestinations() {
+        return destinations;
+    }
 }
